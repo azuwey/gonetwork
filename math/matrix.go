9d9ad3138b646d640ac73db39c14a1e0414e6b90
David Zarandi <david.zarandi@pm.me>
2021-03-07 18:42:16 +0100

    Matrix element-wise operations

--- math/matrix.go
+++ math/matrix.go
@@ -1,13 +1,15 @@
 package math
 
+import "errors"
+
 // Matrix represents a mathematical matrix
 type Matrix struct {
 	values [][]float64
 }
 
-// InitMatrix is initialize a new Matrix
+// InitMatrix is initialize a new Matrix with 0 values
 func InitMatrix(rows, cols int) *Matrix {
-	matrix := new(Matrix)
+	matrix := &Matrix{}
 	matrix.values = make([][]float64, rows)
 
 	for rowIndex := range matrix.values {
@@ -17,20 +19,52 @@ func InitMatrix(rows, cols int) *Matrix {
 	return matrix
 }
 
-// Add is add the summand value to all the values in the matrix
-func (m *Matrix) Add(summand float64) {
-	for rowIndex, row := range m.values {
-		for colIndex := range row {
-			m.values[rowIndex][colIndex] += summand;
+// Add is sum the values of the original matrix and the summand.
+// The summand can be a *Matrix with the same dimensions as the original matrix,
+// or it can be a float64 number
+func (m *Matrix) Add(summand interface{}) error {
+	if summandMatrix, isMatrix := summand.(*Matrix); isMatrix {
+		for rowIndex, row := range m.values {
+			for colIndex := range row {
+				m.values[rowIndex][colIndex] += summandMatrix.values[rowIndex][colIndex];
+			}
 		}
+		return nil;
 	}
+
+	if summandFloat, isFloat := summand.(float64); isFloat {
+		for rowIndex, row := range m.values {
+			for colIndex := range row {
+				m.values[rowIndex][colIndex] += summandFloat;
+			}
+		}
+		return nil;
+	}
+
+	return errors.New("Not supported summand type, should be float64 or *Matrix")
 }
 
-// Multiply is multiply all the values in the matrix by the given multiplier
-func (m *Matrix) Multiply(multiplier float64) {
-	for rowIndex, row := range m.values {
-		for colIndex := range row {
-			m.values[rowIndex][colIndex] *= multiplier;
+// Multiply is multiply the values of the original matrix and the multiplier
+// the multiplier can be a *Matrix with the same dimensions as the original matrix,
+// or it can be a float64 number
+func (m *Matrix) Multiply(multiplier interface{}) error {
+	if multiplierMatrix, isMatrix := multiplier.(*Matrix); isMatrix {
+		for rowIndex, row := range m.values {
+			for colIndex := range row {
+				m.values[rowIndex][colIndex] *= multiplierMatrix.values[rowIndex][colIndex];
+			}
 		}
+		return nil;
 	}
+
+	if multiplierFloat, isFloat := multiplier.(float64); isFloat {
+		for rowIndex, row := range m.values {
+			for colIndex := range row {
+				m.values[rowIndex][colIndex] *= multiplierFloat;
+			}
+		}
+		return nil;
+	}
+
+	return errors.New("Not supported summand type, should be float64 or *Matrix")
 }
--- math/matrix_test.go
+++ math/matrix_test.go
@@ -21,14 +21,14 @@ func TestInit(t *testing.T) {
 
 		for colIndex, col := range row {
 			if col != 0 {
-				t.Logf("value of matrix.values[%d][%d] should be %d", rowIndex, colIndex, 0)
+				t.Logf("value of matrix.values[%d][%d] should be %d, but it's %f", rowIndex, colIndex, 0, col)
 				t.Fail()
 			}
 		}
 	}
 }
 
-func TestAdd(t *testing.T) {
+func TestAddFloat(t *testing.T) {
 	rows := 2
 	cols := 3
 	summand := 5.0
@@ -39,14 +39,33 @@ func TestAdd(t *testing.T) {
 	for rowIndex, row := range matrix.values {
 		for colIndex, col := range row {
 			if col != summand {
-				t.Logf("value of matrix.values[%d][%d] should be %f", rowIndex, colIndex, summand)
+				t.Logf("value of matrix.values[%d][%d] should be %f, but it's %f", rowIndex, colIndex, summand, col)
 				t.Fail()
 			}
 		}
 	}
 }
 
-func TestMultiply(t *testing.T) {
+func TestAddMatrix(t *testing.T) {
+	rows := 2
+	cols := 3
+	summand := InitMatrix(rows, cols)
+	summand.Add(10.0)
+
+	matrix := InitMatrix(rows, cols)
+	matrix.Add(summand);
+
+	for rowIndex, row := range matrix.values {
+		for colIndex, col := range row {
+			if col != summand.values[rowIndex][colIndex] {
+				t.Logf("value of matrix.values[%d][%d] should be %f, but it's %f", rowIndex, colIndex, summand.values[rowIndex][colIndex], col)
+				t.Fail()
+			}
+		}
+	}
+}
+
+func TestMultiplyFloat(t *testing.T) {
 	rows := 2
 	cols := 3
 	newValue := 5.0
@@ -65,7 +84,35 @@ func TestMultiply(t *testing.T) {
 	for rowIndex, row := range matrix.values {
 		for colIndex, col := range row {
 			if col != newValue * multiplier {
-				t.Logf("value of matrix.values[%d][%d] should be %f", rowIndex, colIndex, newValue * multiplier)
+				t.Logf("value of matrix.values[%d][%d] should be %f, but it's %f", rowIndex, colIndex, newValue * multiplier, col)
+				t.Fail()
+			}
+		}
+	}
+}
+
+func TestMultiplyMatrix(t *testing.T) {
+	rows := 2
+	cols := 3
+	newValue := 5.0
+	newValueMultiplier := 3.0
+	multiplier := InitMatrix(rows, cols)
+	multiplier.Add(newValueMultiplier)
+
+	matrix := InitMatrix(rows, cols)
+
+	for rowIndex, row := range matrix.values {
+		for colIndex := range row {
+			matrix.values[rowIndex][colIndex] = newValue
+		}
+	}
+
+	matrix.Multiply(multiplier);
+
+	for rowIndex, row := range matrix.values {
+		for colIndex, col := range row {
+			if col != newValue * newValueMultiplier {
+				t.Logf("value of matrix.values[%d][%d] should be %f, but it's %f", rowIndex, colIndex, newValue * newValueMultiplier, col)
 				t.Fail()
 			}
 		}
